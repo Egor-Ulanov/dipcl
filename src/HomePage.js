44e4047d2ec754ff06b2c0f35ae9ec52941d3e44
Egor <egor-ulanov@mail.ru>
2025-05-26 23:01:48 +0500

    Новая базовая структура приложения1

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,6 @@
 // src/components/HomePage.jsx
 import React, { useState, useEffect } from "react";
-import { auth, db } from "../firebase";
+import { auth, db } from "./firebaseConfig";
 import {
   doc,
   getDoc,
