0fc08d294709331731fa0ebf2265170fb31f1d14
Egor <egor-ulanov@mail.ru>
2025-06-10 15:01:03 +0500

    Внешность14

--- src/components/ChangeCredentials.js
+++ src/components/ChangeCredentials.js
@@ -69,27 +69,30 @@ function ChangeCredentials() {
   };
 
   return (
-    <div className="page-container">
-      <div className="page-header">
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12ZM12 14C9.33 14 4 15.34 4 18V20H20V18C20 15.34 14.67 14 12 14Z" fill="currentColor"/>
-        </svg>
-        <h1>Изменение учетных данных</h1>
-      </div>
+    <div className="credentials-page">
+      <div className="credentials-content">
+        <div className="credentials-header">
+          <div className="header-icon">
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12ZM12 14C9.33 14 4 15.34 4 18V20H20V18C20 15.34 14.67 14 12 14Z" fill="currentColor"/>
+            </svg>
+          </div>
+          <h1>Изменение учетных данных</h1>
+        </div>
 
-      <div className="change-credentials-container">
-        <div className="change-credentials-card">
-          <form onSubmit={handleSubmit} className="credentials-form">
-            <div className="form-group">
-              <label htmlFor="currentPassword">
-                <svg className="input-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M18 8H17V6C17 3.24 14.76 1 12 1C9.24 1 7 3.24 7 6V8H6C4.9 8 4 8.9 4 10V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V10C20 8.9 19.1 8 18 8ZM12 17C10.9 17 10 16.1 10 15C10 13.9 10.9 13 12 13C13.1 13 14 13.9 14 15C14 16.1 13.1 17 12 17ZM15.1 8H8.9V6C8.9 4.29 10.29 2.9 12 2.9C13.71 2.9 15.1 4.29 15.1 6V8Z" fill="currentColor"/>
-                </svg>
+        <form onSubmit={handleSubmit} className="credentials-form">
+          <div className="form-section">
+            <div className="input-group">
+              <label>
+                <span className="label-icon">
+                  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18 8H17V6C17 3.24 14.76 1 12 1C9.24 1 7 3.24 7 6V8H6C4.9 8 4 8.9 4 10V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V10C20 8.9 19.1 8 18 8Z" fill="currentColor"/>
+                  </svg>
+                </span>
                 Текущий пароль
               </label>
               <input
                 type="password"
-                id="currentPassword"
                 name="currentPassword"
                 value={formData.currentPassword}
                 onChange={handleChange}
@@ -98,16 +101,17 @@ function ChangeCredentials() {
               />
             </div>
 
-            <div className="form-group">
-              <label htmlFor="newUsername">
-                <svg className="input-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.29 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.97 13.99 18 15.98C16.71 17.92 14.5 19.2 12 19.2Z" fill="currentColor"/>
-                </svg>
+            <div className="input-group">
+              <label>
+                <span className="label-icon">
+                  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5Z" fill="currentColor"/>
+                  </svg>
+                </span>
                 Новый логин
               </label>
               <input
                 type="text"
-                id="newUsername"
                 name="newUsername"
                 value={formData.newUsername}
                 onChange={handleChange}
@@ -115,16 +119,17 @@ function ChangeCredentials() {
               />
             </div>
 
-            <div className="form-group">
-              <label htmlFor="newPassword">
-                <svg className="input-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12.65 10C11.83 7.67 9.61 6 7 6C3.69 6 1 8.69 1 12C1 15.31 3.69 18 7 18C9.61 18 11.83 16.33 12.65 14H17V18H21V14H23V10H12.65ZM7 14C5.9 14 5 13.1 5 12C5 10.9 5.9 10 7 10C8.1 10 9 10.9 9 12C9 13.1 8.1 14 7 14Z" fill="currentColor"/>
-                </svg>
+            <div className="input-group">
+              <label>
+                <span className="label-icon">
+                  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12.65 10C11.83 7.67 9.61 6 7 6C3.69 6 1 8.69 1 12C1 15.31 3.69 18 7 18C9.61 18 11.83 16.33 12.65 14H17V18H21V14H23V10H12.65Z" fill="currentColor"/>
+                  </svg>
+                </span>
                 Новый пароль
               </label>
               <input
                 type="password"
-                id="newPassword"
                 name="newPassword"
                 value={formData.newPassword}
                 onChange={handleChange}
@@ -132,37 +137,38 @@ function ChangeCredentials() {
               />
             </div>
 
-            <div className="form-group">
-              <label htmlFor="confirmPassword">
-                <svg className="input-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M18 8H17V6C17 3.24 14.76 1 12 1C9.24 1 7 3.24 7 6V8H6C4.9 8 4 8.9 4 10V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V10C20 8.9 19.1 8 18 8ZM12 17C10.9 17 10 16.1 10 15C10 13.9 10.9 13 12 13C13.1 13 14 13.9 14 15C14 16.1 13.1 17 12 17ZM15.1 8H8.9V6C8.9 4.29 10.29 2.9 12 2.9C13.71 2.9 15.1 4.29 15.1 6V8Z" fill="currentColor"/>
-                </svg>
+            <div className="input-group">
+              <label>
+                <span className="label-icon">
+                  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18 8H17V6C17 3.24 14.76 1 12 1C9.24 1 7 3.24 7 6V8H6C4.9 8 4 8.9 4 10V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V10C20 8.9 19.1 8 18 8Z" fill="currentColor"/>
+                  </svg>
+                </span>
                 Подтверждение пароля
               </label>
               <input
                 type="password"
-                id="confirmPassword"
                 name="confirmPassword"
                 value={formData.confirmPassword}
                 onChange={handleChange}
                 placeholder="Подтвердите новый пароль"
               />
             </div>
+          </div>
 
-            <button type="submit" className="submit-button" disabled={loading}>
-              {loading ? (
-                <span className="loading-spinner"></span>
-              ) : (
-                <>
-                  <svg className="button-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 16.2L4.8 12L3.4 13.4L9 19L21 7L19.6 5.6L9 16.2Z" fill="currentColor"/>
-                  </svg>
-                  Сохранить изменения
-                </>
-              )}
-            </button>
-          </form>
-        </div>
+          <button type="submit" className="submit-button" disabled={loading}>
+            {loading ? (
+              <span className="loading-spinner"></span>
+            ) : (
+              <>
+                <svg className="button-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M9 16.2L4.8 12L3.4 13.4L9 19L21 7L19.6 5.6L9 16.2Z" fill="currentColor"/>
+                </svg>
+                <span>Сохранить изменения</span>
+              </>
+            )}
+          </button>
+        </form>
       </div>
     </div>
   );
